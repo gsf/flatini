9609e26e08a3cf27b2d98427de1624fd7a835205
Gabriel Farrell <g@grrawr.com>
2014-06-06 03:06:26 -0400

    Cleanup

--- index.js
+++ index.js
@@ -1,6 +1,44 @@
-var eol = process.platform === "win32" ? "\r\n" : "\n"
+function isQuoted (val) {
+  return (val.charAt(0) === "\"" && val.slice(-1) === "\"")
+         || (val.charAt(0) === "'" && val.slice(-1) === "'")
+}
 
-module.exports = function decode (str) {
+function unsafe (val, doUnesc) {
+  val = (val || "").trim()
+  if (isQuoted(val)) {
+    // remove the single quotes before calling JSON.parse
+    if (val.charAt(0) === "'") {
+      val = val.substr(1, val.length - 2);
+    }
+    try { val = JSON.parse(val) } catch (_) {}
+  } else {
+    // walk the val to find the first not-escaped ; character
+    var esc = false
+    var unesc = "";
+    for (var i = 0, l = val.length; i < l; i++) {
+      var c = val.charAt(i)
+      if (esc) {
+        if ("\\;#".indexOf(c) !== -1)
+          unesc += c
+        else
+          unesc += "\\" + c
+        esc = false
+      } else if (";#".indexOf(c) !== -1) {
+        break
+      } else if (c === "\\") {
+        esc = true
+      } else {
+        unesc += c
+      }
+    }
+    if (esc)
+      unesc += "\\"
+    return unesc
+  }
+  return val
+}
+
+module.exports = function parse (str) {
   var out = {}
     , p = out
     , section = null
@@ -49,44 +87,4 @@ module.exports = function decode (str) {
   })
 
   return out
-}
-
-function isQuoted (val) {
-  return (val.charAt(0) === "\"" && val.slice(-1) === "\"")
-         || (val.charAt(0) === "'" && val.slice(-1) === "'")
-}
-
-function unsafe (val, doUnesc) {
-  val = (val || "").trim()
-  if (isQuoted(val)) {
-    // remove the single quotes before calling JSON.parse
-    if (val.charAt(0) === "'") {
-      val = val.substr(1, val.length - 2);
-    }
-    try { val = JSON.parse(val) } catch (_) {}
-  } else {
-    // walk the val to find the first not-escaped ; character
-    var esc = false
-    var unesc = "";
-    for (var i = 0, l = val.length; i < l; i++) {
-      var c = val.charAt(i)
-      if (esc) {
-        if ("\\;#".indexOf(c) !== -1)
-          unesc += c
-        else
-          unesc += "\\" + c
-        esc = false
-      } else if (";#".indexOf(c) !== -1) {
-        break
-      } else if (c === "\\") {
-        esc = true
-      } else {
-        unesc += c
-      }
-    }
-    if (esc)
-      unesc += "\\"
-    return unesc
-  }
-  return val
 }
